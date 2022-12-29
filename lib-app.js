182055ef28801b2ce2248ff7b7a282d326f30b94
Ronnie <ronniebercero@gmail.com>
2022-12-29 22:40:05 +0800

    Add book object constructor

--- lib-app.js
+++ lib-app.js
@@ -1,7 +1,26 @@
 let myLibrary = [];
 
-function Book() {
+function Book(
+    title = 'Unknown',
+    author = 'Unknown',
+    pages = '0',
+    read = '0',
+    isRead = false,
+    isReading = false,
+    cover = '',
+    bgColor = '',
+    id = ''
+) {
     // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.isRead = isRead;
+    this.isReading = isReading;
+    this.cover = cover;
+    this.bgColor = bgColor;
+    this.id = id;
 }
 
 function addBookToLibrary() {
