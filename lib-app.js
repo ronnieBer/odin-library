e598040ee60287a53e68b4d48a2cbefbbadc5d1e
Ronnie <ronniebercero@gmail.com>
2023-01-05 00:13:17 +0800

    Add function to allow numbers only to pages and read input

--- lib-app.js
+++ lib-app.js
@@ -348,6 +348,21 @@ function resetBookshelves() {
     addNewBook();
 }
 
+// Function to allow numbers only
+function inputNumOnly(e) {
+    const theEvent = e || window.event;
+    let key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    const regex = /[0-9]/;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
+bookFormInput[2].addEventListener("keypress", (e) => inputNumOnly(e)); // Pages input
+bookFormInput[3].addEventListener("keypress", (e) => inputNumOnly(e)); // Read input
+
 // Book form event listener at submit
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
